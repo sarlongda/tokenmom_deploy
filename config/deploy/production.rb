f538734386be7fd421bc1e9096141f0d16f8404d
sarlongda <sarlongda@gmail.com>
2018-09-13 18:55:14 +0700

    deploy fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server '178.128.31.172', user: 'deploy', roles: %w{web app}
+server '178.128.31.172', user: 'deploy', roles: %w{web app db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
