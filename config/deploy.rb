326adfdbf2c849dfea2a3c14c6fda0c8fd80a9e2
sarlongda <sarlongda@gmail.com>
2018-09-13 23:16:44 +0700

    Passenger automatic deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ lock "~> 3.11.0"
 set :application, "TokenMom"
 set :repo_url, "git@github.com:sarlongda/tokenmom_deploy.git"
 
+set :passenger_restart_with_touch, true
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
