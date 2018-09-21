02de4c23d24c79cc831ddb7436bea114eb7c0344
sarlongda <sarlongda@gmail.com>
2018-09-22 01:05:34 +0700

    cable fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.action_cable.url = 'ws://54.251.164.163/cable'
+  config.action_cable.url = 'wss://54.251.164.163/cable'
   config.action_cable.disable_request_forgery_protection = true
   config.cache_classes = false
 
