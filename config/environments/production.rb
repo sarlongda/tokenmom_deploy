7cfa1b3ba32e8783a1bf70537e55afc3b6f78bac
sarlongda <sarlongda@gmail.com>
2018-09-24 10:59:49 +0700

    landing page image change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_cable.url = 'ws://54.251.164.163:80/cable'
+  config.action_cable.url = 'wss://54.251.164.163/cable'
+  config.action_cable.disable_request_forgery_protection = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
