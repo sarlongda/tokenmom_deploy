4f17c22249abae3c7f46a7de967bf47917cb1977
sarlongda <sarlongda@gmail.com>
2018-10-03 02:03:06 +0700

    cable fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,10 +3,10 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  # config.action_cable.url = '/cable'
-  config.action_cable.url = 'wss://tokenmom.com/cable'
-  config.action_cable.disable_request_forgery_protection = true
-  config.action_cable.allowed_request_origins = [/http:\/\/*/,/https:\/\/*/]
+  config.action_cable.url = '/cable'
+  # config.action_cable.url = 'wss://tokenmom.com/cable'
+  # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = ["https://tokenmom.com"]
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
