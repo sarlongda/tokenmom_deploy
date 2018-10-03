be7aff36be47d726c5ae70abfb57724cea19eeb0
sarlongda <sarlongda@gmail.com>
2018-10-03 11:08:07 +0700

    order broadcast job fix

--- app/jobs/order_broadcast_job.rb
+++ app/jobs/order_broadcast_job.rb
@@ -1,16 +1,9 @@
 class OrderBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(type,order = "")
+  def perform(type, order)
     # Do something later
-    
-    if type == "add"
-      ActionCable.server.broadcast 'order_channel', order: order, type: type
-    elsif type == "remove"
-      ActionCable.server.broadcast 'order_channel', type: type,order: order
-    elsif type == "update"
-      ActionCable.server.broadcast 'order_channel', type: type, order: order
-    end  
-   
+    logger.debug("#####################Order_BroadCast")
+    ActionCable.server.broadcast 'order_channel', order: order, type: type
   end
 end
